fae57b321e33fbc88a11e4ff06326c08cc5b6df9
Fauzee26 <32213481+Fauzee26@users.noreply.github.com>
2018-09-26 21:17:50 +0700

    Initial commit

---
+++ app/src/main/java/fauzi/hilmy/ojeklah/helper/MyViewHolder.java
@@ -0,0 +1,12 @@
+package fauzi.hilmy.ojeklah.helper;
+
+import android.view.View;
+
+import com.orhanobut.dialogplus.ViewHolder;
+
+public class MyViewHolder extends ViewHolder{
+
+    public MyViewHolder(View contentView) {
+        super(contentView);
+    }
+}
